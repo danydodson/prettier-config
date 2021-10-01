0a316432cc377b1abf57a53b631f1362df9e4eb0
Dany Dodson <danydodson@gmail.com>
2021-10-01 01:54:23 -0500

    feat: :wrench:

---
+++ index.js
@@ -0,0 +1,13 @@
+// https://prettier.io/docs/en/configuration.html
+
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+};
